34d2192540eeb7fd946db13c5a0040a0a5032568
Greg McMullen <mcmullen.greg@gmail.com>
2021-07-26 16:03:51 -0400

    Structural stuffs

---
+++ includes/head.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title><?= $title; ?></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="/public/css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
